23a7d30873f84cdc1d1b9a58e5c5395ad221ae5f
slmnkankal <slmnknkl@gmail.com>
2022-08-17 12:30:12 +0300

    note model is created

--- api/models.py
+++ api/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Note(models.Model):
+    body = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True) # in evey update!
+    created = models.DateTimeField(auto_now_add=True) #when created!
 
+    def __str__(self) :
+        return self.body[0:50] # first fifty character!
