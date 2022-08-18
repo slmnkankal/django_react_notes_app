0dfa62bb35b7134d3bee0cdb420dd31bdd0eb9c5
slmnkankal <slmnknkl@gmail.com>
2022-08-18 18:18:24 +0300

    finished crud fnctionality

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Note
 from .serializers import NoteSerializer
+from api import serializers
 
 
 @api_view(['GET'])
@@ -46,12 +47,41 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getNotes(request):
-    notes = Note.objects.all()
+    notes = Note.objects.all().order_by('-updated') # ordered based on updated field of model
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def getNote(request, pk): # to get single object getNote
-    notes = Note.objects.get(id=pk) # all to get
+    notes = Note.objects.get(id=pk) # changed from all to get
     serializer = NoteSerializer(notes, many=False) # many True to False
     return Response(serializer.data)
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note = Note.objects.create(
+        body=data['body']
+    )
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response('Note was deleted!')
+
--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,33 +1,82 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
-import { Link } from 'react-router-dom'
+
+import { useNavigate } from "react-router-dom";
 
 const NotePage = () => {
 
-    const { id } = useParams()
+    const navigate = useNavigate();
+    const params = useParams()
     let [note, setNote] = useState(null)
 
     useEffect(() => {
         getNote()
-    }, [id])
+    }, [params.id])
 
     let getNote = async () => {
-        let response = fetch(`/api/notes/${id}`)
+      if (params.id === 'new') return
+        let response = fetch(`/api/notes/${params.id}`)
         let data = await (await response).json()
         setNote(data)
+    }
+
+    let createNote = async () => {
+      fetch(`/api/notes/create/`, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(note)
+      })
+
+    }
+    let updateNote = async () => {
+      fetch(`/api/notes/${params.id}/update/`, {
+          method: "PUT",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(note)
+      })
+    }
+
+    let deleteNote = async () => {
+      fetch(`/api/notes/${params.id}/delete/`, {
+        method:'DELETE',
+        'headers':{
+          'Content-Type': 'application/json'
+        }
+      })
+      navigate('/')
+    }
+
+    let handleSubmit = () => {
+      console.log('NOTE:', note)
+      if (params.id !== 'new' && note.body == "") {
+        deleteNote()
+      } else if (params.id !== 'new') {
+        updateNote()
+      } else if (params.id == 'new' && note !== null) {
+        createNote()
+      }
+      navigate('/')
     }
 
   return (
     <div className='note'>
       <div className="note-header">
         <h3>
-          <Link to='/'>
-        < ArrowLeft />
-        </Link>
+        < ArrowLeft onClick={handleSubmit}/>
         </h3>
+        { params.id !== 'new' ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={handleSubmit}>Done</button>
+
+        )}
       </div>
-        <textarea defaultValue={note?.body}></textarea>
+        <textarea onChange={(e) => { setNote({...note, 'body': e.target.value})}} value={note?.body}></textarea>
     </div>
   )
 }
